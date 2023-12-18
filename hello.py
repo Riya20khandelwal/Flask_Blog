5da35264dc123314e4248e4eb7283e190b13e104
Riya20khandelwal <riyakhandelwal204@gmail.com>
2023-12-18 21:50:47 +0530

    Form Class

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from impdata import secret_key
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+
+# Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("Full Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
@@ -11,6 +21,17 @@ def index():
 def user(name):
     # return "<h1>Hello {}!</h1>".format(name)
     return render_template("user.html",user_name=name)
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = UserForm()
+
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
 
 # Create Custom Error Pages
 
@@ -23,3 +44,4 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
