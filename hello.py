511f1554bd250756e133e23e43d7aba8f7952bdd
Riya20khandelwal <riyakhandelwal204@gmail.com>
2023-12-26 20:34:45 +0530

    Show Posts

--- hello.py
+++ hello.py
@@ -112,7 +112,10 @@ def add_post():
 
     return render_template("add_post.html",
                            form=form)
-
+@app.route('/posts')
+def posts():
+    posts = Posts.query.order_by(Posts.date_posted)
+    return render_template('posts.html', posts=posts)
 
 @app.route('/')
 def index():
